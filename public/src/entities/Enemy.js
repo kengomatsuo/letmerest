6ff5d89b9dba764dd3384eb3326e5e017cbd7eac
kengomatsuo <kennethfang1000@gmail.com>
2025-03-18 17:55:12 +0700

    Updated enemy damage into 8

--- public/src/entities/Enemy.js
+++ public/src/entities/Enemy.js
@@ -5,7 +5,7 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
     scene.physics.add.existing(this);
 
     this.speed = 100;
-    this.damage = 4;
+    this.damage = 8;
     this.maxSpeed = 100;
     this.acceleration = 5;
     this.health = 50;
